4ec7cedc46858c957a8160f7ac3484f1417879bb
Julien Caussade <julien.caussade@gmail.com>
2024-03-11 15:45:25 +0100

    Creating part of design and javascript

---
+++ script.js
@@ -0,0 +1,23 @@
+const hoursHand = document.querySelector("#hours");
+const minutesHand = document.querySelector("#minutes");
+const secondsHand = document.querySelector("#seconds");
+
+setInterval(movement, 1000);
+
+function movement() {
+    const time = new Date();
+
+    const hours = time.getHours();
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+
+    const angleHours = 30 * hours;
+    hoursHand.style = `transform: rotate(${angleHours}deg)`;
+
+    const angleMinutes = 360 / (60 / minutes);
+    minutesHand.style = `transform: rotate(${angleMinutes}deg)`;
+
+    const angleSeconds = 360 / (60 / seconds);
+    secondsHand.style = `transform: rotate(${angleSeconds}deg)`;
+}
+
